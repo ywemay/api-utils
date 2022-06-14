3557378a9e56f66c84766a2559f3dfb63193f3f0
Dorian Chibuc <devel@ywemay.com>
2022-06-14 18:16:07 +0800

    Modifying the pagination mode

--- pagination.js
+++ pagination.js
@@ -1,17 +1,28 @@
-exports.reqPage = (req, docs_count = 0) => {
-  const size = req.query.page_size ? parseInt(req.query.page_size) : 50;
-  const nr = (req.query.pg ? parseInt(req.query.pg) : 1) - 1;
+exports.reqPage = function(req, defaultPerPage = 25) {
+  let { page, perPage } = req.query || { page: 1, perPage: defaultPerPage };
+  perPage = parseInt(perPage || defaultPerPage);
+  pageNr = parseInt(page || 1) -1;
   return {
-    size, nr: nr,
-    skip: size * nr,
-    limit: size,
-    docs_count
+    perPage,
+    page,
+    skip: perPage * pageNr,
+    limit: perPage
   }
 }
 
-exports.reqPageBack = function (req) {
-  return {
-    size: req.query.page_size ? parseInt(req.query.page_size) : 50,
-    nr: (req.query.pg ? parseInt(req.query.pg) : 1) - 1
+exports.reqSort = function(req, defaultSort = false) {
+  let { sort } = req.query || { sort: false }
+  if (!sort) return defaultSort;
+  const rez = {};
+  if (typeof sort === 'string' && sort.match(/[\w\.\_]+/)) { 
+    rez[sort] = 1;
   }
+  if (typeof sort === 'object') {
+    return Object.keys(sort).reduce((acc, key) => {
+      const dir = parseInt(sort[key])
+      if (dir * dir === 1) acc[key] = dir;
+      return acc;
+    }, {});
+  }
+  return defaultSort;
 }
